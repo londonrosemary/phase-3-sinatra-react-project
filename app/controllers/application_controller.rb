6eb0c30a9c0f0bb21fa32ae878b1604c349d3d46
London <london.rosemary@gmail.com>
2021-10-07 12:59:20 -0400

    Backend sign up and sign in functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < Sinatra::Base
+  enable :sessions
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+
+
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,15 @@ class UsersController < ApplicationController
     get "/users" do
         User.all.to_json
     end    
-
+    
     post "/users" do
         user = User.create(first_name: params[:firstName], last_name: params[:lastName], user_name: params[:username], email: params[:email], password: params[:password])
         user.to_json
     end
-    
+
+    post "/login" do
+        # binding.pry
+        login = User.find_by(email: params[:email],password: params[:password] )
+        login.to_json
+    end
 end
